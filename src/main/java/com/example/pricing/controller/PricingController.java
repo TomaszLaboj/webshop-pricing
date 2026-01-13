c1a32b149f7563bc640b0e46660ca3eafa710306
Tomasz Laboj <tomasz.laboj@ocado.com>
2026-01-13 18:28:52 +0000

    fix kafka

--- src/main/java/com/example/pricing/controller/PricingController.java
+++ src/main/java/com/example/pricing/controller/PricingController.java
@@ -1,14 +1,8 @@
 package com.example.pricing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.pricing.domain.PricingService;
-import com.example.pricing.domain.model.ProductPrice;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 public class PricingController {
--- src/main/java/com/example/pricing/kafka/KafkaProducerConfig.java
+++ src/main/java/com/example/pricing/kafka/KafkaProducerConfig.java
@@ -11,8 +11,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import com.example.pricing.domain.model.ProductsCalculatedPrices;
-
 @Configuration
 public class KafkaProducerConfig {
 
